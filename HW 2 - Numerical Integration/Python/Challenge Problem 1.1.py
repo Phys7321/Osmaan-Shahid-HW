349dff6d2f627ac6fbd17cc3b412d15438da1f1d
osmaanshahid <shahid.o@husky.neu.edu>
2018-09-23 04:47:46 -0400

    Almost finished Problem 1.py

--- HW 2 - Numerical Integration/Python/Challenge Problem 1.1.py
+++ HW 2 - Numerical Integration/Python/Challenge Problem 1.1.py
@@ -20,7 +20,7 @@ def t_vs_a(a,N):
     x = np.linspace(0,a,N)              # X-axis is the amplitude
     y = np.zeros(N)                     # Y-axis is the period
     for i in range(1,N):                # N is the number of data points
-        integrand = lambda x: (4/math.sqrt(math.pi))/(math.sqrt( V(a*(i/N)) - V(x) ))    # Fxn to be integrated
+        integrand = lambda z: (4/math.sqrt(math.pi))/(math.sqrt( V(a*(i/N)) - V(z) ))    # Fxn to be integrated
         y[i-1] = sci.romberg(integrand,0,a*(i/N)-0.0001,divmax=20)         # Y-axis is integral of fxn
     plt.plot(x,y)
     plt.xlabel('Amplitude')
--- HW 2 - Numerical Integration/Python/Problem 1.py
+++ HW 2 - Numerical Integration/Python/Problem 1.py
@@ -9,18 +9,21 @@ import numpy as np
 # Linear charge density
 L = lambda x: 2*x
 
-def FindPotential():
-    xprime = np.linspace(0,1,10) # Describes x-coordinate of charge distribution
-    yprime = np.linspace(0,0,10) # Describes y-coordinate of charge distribution
-    x = np.linspace(-2,3,10) # Describes x-coordinate in space
-    y = np.linspace(-2,2,10) # Describes y-coordinate in space
-    for j in range(0,10):           # Sets the y-coordinate 
-        for i in range(0,10):       # Sets the x-coordinate
-            L = lambda xprime: 2*xprime[i]
-            x_dist = x[j] - xprime[i]
-            y_dist = y[j] - yprime[i]
-            dist = math.sqrt( x_dist**2 + y_dist**2 )
-            V[i,j] = sci.romberg(L/dist,0,1)
-
-
-
+def FindPotential(N):
+    V = [[0]*N for i in range(N)] # Potential is 2D array with 10 columns, 10 rows
+    # xprime = np.linspace(0,1,10) # Describes x-coordinate of charge distribution
+    # yprime = np.linspace(0,0,10) # Describes y-coordinate of charge distribution
+    x = np.linspace(-0.5,1.5,N) # Describes x-coordinate in space
+    y = np.linspace(-0.5,0.5,N) # Describes y-coordinate in space
+    for j in range((N-1),-1,-1):           # Decreases in value 
+        for i in range(0,N):               # Increases in value
+            integrand = lambda z: 2*z/math.sqrt((x[i] - z)**2 + (y[j] - 0)**2)
+            # x_dist = x[j] - xprime[i]
+            # y_dist = y[j] - yprime[i]
+            # dist = math.sqrt( x_dist**2 + y_dist**2 )
+            V[i][j] = sci.romberg(integrand,0,1,divmax=20)
+    cs = plt.pcolormesh(np.transpose(V))
+    #cs = plt.pcolor(np.transpose(V))
+    cb = plt.colorbar(cs, orientation = 'horizontal')
+    plt.show()
+    return V
