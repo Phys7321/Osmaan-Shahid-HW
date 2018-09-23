00fdf17d9e510300e1e17fde451e13059c7bc6bb
osmaanshahid <shahid.o@husky.neu.edu>
2018-09-23 00:23:46 -0400

    Finished Challenge Problem 1.1.

--- HW 2 - Numerical Integration/Challenge Problem 1.1.py
+++ HW 2 - Numerical Integration/Challenge Problem 1.1.py
@@ -12,17 +12,25 @@ V = lambda x: x**4
 a = 4
 m = 1
 
-# Integrand
+# Integrand, to be used in the function
 integrand = lambda x: (4/math.sqrt(math.pi))/(math.sqrt( V(a) - V(x) ))
 
+# a_vs_t will calculate the period T for each amplitude a
 def a_vs_t(a,N):
-    x = np.linspace(0,a,N)
-    y = np.zeros(N)
-    for i in range(0,N):
-        integrand = lambda x: (4/math.sqrt(math.pi))/(math.sqrt( V(a*(1/N)-0.01) - V(x) ))
-        y[i] = sci.romberg(integrand,0,a*(i/N)-0.01)
+    x = np.linspace(0,a,N)              # X-axis is the amplitude
+    y = np.zeros(N)                     # Y-axis is the period
+    for i in range(0,N):                # N is the number of data points
+        integrand = lambda x: (4/math.sqrt(math.pi))/(math.sqrt( V(a*(i/N)+0.01) - V(x) ))    # Fxn to be integrated
+        y[i] = sci.romberg(integrand,0,a*(i/N),divmax=20)         # Y-axis is integral of fxn
     plt.plot(x,y)
     plt.xlabel('Amplitude')
     plt.ylabel('Period')
     plt.show()
+    plt.suptitle('T vs. A for V(x) = x^4')
     return x,y;
+
+
+# Define amplitude and number of points
+a = 4
+N = 500
+a_vs_t(a,N)
