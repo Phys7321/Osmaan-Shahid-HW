8b498aeb8e13b4dd401a4e13536ebcbe1af2d3fb
osmaanshahid <shahid.o@husky.neu.edu>
2018-10-07 19:37:16 -0400

    Updated code on Problem 2.3

--- HW3_DifferentialEquations/ChallengeProblems/ChallengeProblem2_3.py
+++ HW3_DifferentialEquations/ChallengeProblems/ChallengeProblem2_3.py
@@ -40,16 +40,19 @@ v0 = 0.            # initial velocity
 vt = 30.           # terminal velocity
 k2 = g*mass/vt**2  # drag coeff.
 
-dt = 0.1           # time step
+dt = 0.01           # time step
 
 
 p = particle(mass, y0, v0)
 
 gforce = g*mass/(1+p.y/R)**2    # weight
 
+
 y = [y0] # since we do not know the size of the arrays, we define first a python list
 v = [v0] # the append method is more efficient for lists than arrays
 t = [0.]
+
+vnormal = [v0] # speed for which a = -g the whole time
 
 while p.y > 0.:
     fy = -gforce-k2*p.v*abs(p.v)
@@ -57,10 +60,13 @@ while p.y > 0.:
     y.append(p.y)
     v.append(p.v)
     t.append(t[-1]+dt)
+    vnormal.append(vnormal[-1] - g*dt)
+
     
 t_data = np.array(t) # we convert the list into a numpy array for plotting
 y_data = np.array(y)
 v_data = np.array(v)
+vnormal_data = np.array(vnormal)
 
 #for i in range(0,t_data.size):
 #    print (i,t_data[i], y_data[i], v_data[i])
@@ -70,8 +76,38 @@ plt.xlabel('Time (s)')
 plt.ylabel('Velocity (m/s)')
 plt.show()
 
+plt.plot(t_data, vnormal_data, color="#FF0000", ls='-', lw=3)
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (m/s) (constant g)')
+plt.show()
+
 plt.plot(t_data, y_data, color="#0000FF", ls='-', lw=3)
 plt.xlabel('Time (s)')
 plt.ylabel('Position (m)')
 plt.show()
+
+
+p = particle(mass, y0, v0)
+
+y0 = list(np.linspace(1,500,500))
+v = [v0]
+t = [0.]
+vnormal = [v0]
+
+for i in range(0,len(y0)):
+    p = particle(mass,y0[i],v0)
+    y = [y0[i]]
+    while p.y > 0:
+        fy = -gforce-k2*p.v*abs(p.v)
+        p.euler(fy, dt)
+        y.append(p.y)
+        v.append(p.v)
+        t.append(t[-1]+dt)
+        vnormal.append(vnormal[-1] - g*dt)
+    vimpact = v[-1]
+    vnormalimpact = vnormal[-1]
+    diff = np.abs(((vimpact - vnormalimpact)/(vimpact)))*100
+    if diff > 1.0 and diff < 1.9:
+        print(i,diff,vimpact,vnormalimpact,y[i])
+
 
