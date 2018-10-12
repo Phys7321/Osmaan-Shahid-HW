9b74f7ae8dce69c369904babc5d9ef47c8336392
osmaanshahid <shahid.o@husky.neu.edu>
2018-10-12 06:34:01 -0400

    Finished up to Problem 4.

---
+++ HW3_DifferentialEquations/Problem4.m
@@ -0,0 +1,240 @@
+function [period,sol] = Problem4(varargin) 
+% Finds the period of a damped pendulum for large oscillations given the
+% length of the pendulum arm and initial conditions. All angles in radians.
+% Use the input "Problem2(L,theta0,thetad0)" where
+% L = length of pendulum
+% theta0 = initial angle
+
+
+%Setting initial conditions
+switch nargin
+    case 0
+        error('Must input length and initial conditions')
+    case 1
+       L = varargin{1};
+end
+
+g=9.81;
+A = 1 ;          % Amplitude of driving force
+omega0 = sqrt(g/L);
+T0= 2*pi/omega0; % Period of simple harmonic oscillator
+N = 15;           % Number of oscillations to graph
+m = 1;           % Mass of pendulum bob
+gamma1 = 0.5 ;   % Damping factor
+gamma2 = 1.5 ;   % Damping factor
+
+
+% Solve differential equation for different driving force frequencies
+omega = [0 1 2 2.2 2.4 2.6 2.8 3.0 3.2 3.4] ;
+
+tspan = [0 N*T0];                               % Integration time goes from 0 to N*T0
+opts = odeset('refine',6);  % Set up options
+
+theta0 = 1.0 ;         % Initial angle
+thetad0 = 0.0 ;        % Initial angular velocity
+r0 = [theta0 thetad0]; % Initial conditions
+
+% Solve the ODE
+[t1,w1] = ode45(@proj,tspan,r0,opts,g,L,gamma1,A,omega(3));
+
+theta0 = 0.0 ;         % Initial angle
+thetad0 = 1.0 ;        % Initial angular velocity
+r0 = [theta0 thetad0]; % Initial conditions
+
+% Solve the ODE
+[t2,w2] = ode45(@proj,tspan,r0,opts,g,L,gamma1,A,omega(3));
+
+ind1 = find(w1(:,2).*circshift(w1(:,2), [-1 0]) <= 0);
+ind1 = chop(ind1,4);
+period1 = 2*mean(diff(t1(ind1)));  % Find the period
+
+ind2 = find(w2(:,2).*circshift(w2(:,2), [-1 0]) <= 0);
+ind2 = chop(ind2,4);
+period2 = 2*mean(diff(t2(ind2)));  % Find the period
+
+
+% Create plots
+
+figure(1)
+subplot(1,1,1)
+plot(t1,w1(:,1),'k-',t2,w2(:,1),'c-')
+legend('\theta_0 = 1','\theta_0 = 0')
+title('Angle \theta (t) vs. Time')
+xlabel('t')
+ylabel('\theta')
+ylim([-1 1])
+
+
+% From the graph, the steady state solution is a sinusoidal function.
+
+% Now we do these again for different values of omega
+
+
+
+theta0 = 1.0 ;         % Initial angle
+thetad0 = 0.0 ;        % Initial angular velocity
+r0 = [theta0 thetad0]; % Initial conditions
+
+% Solve the ODE
+[t1,w1] = ode45(@proj,tspan,r0,opts,g,L,gamma1,A,omega(2));
+
+theta0 = 0.0 ;         % Initial angle
+thetad0 = 1.0 ;        % Initial angular velocity
+r0 = [theta0 thetad0]; % Initial conditions
+
+% Solve the ODE
+[t2,w2] = ode45(@proj,tspan,r0,opts,g,L,gamma1,A,omega(2));
+
+ind1 = find(w1(:,2).*circshift(w1(:,2), [-1 0]) <= 0);
+ind1 = chop(ind1,4);
+period1 = 2*mean(diff(t1(ind1)));  % Find the period
+
+ind2 = find(w2(:,2).*circshift(w2(:,2), [-1 0]) <= 0);
+ind2 = chop(ind2,4);
+period2 = 2*mean(diff(t2(ind2)));  % Find the period
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% Solve diff. eq. for 10 different initial conditions, gamma1 <--> gamma5
+[t0,w0] = ode45(@proj,tspan,r0,opts,g,L,gamma(1),A,omega);
+[t1,w1] = ode45(@proj,tspan,r0,opts,g,L,gamma(2));
+[t2,w2] = ode45(@proj,tspan,r0,opts,g,L,gamma(3));
+[t3,w3] = ode45(@proj,tspan,r0,opts,g,L,gamma(4));
+[t4,w4] = ode45(@proj,tspan,r0,opts,g,L,gamma(5));
+[t5,w5] = ode45(@proj,tspan,r0,opts,g,L,gamma(6));
+[t6,w6] = ode45(@proj,tspan,r0,opts,g,L,gamma(7));
+[t7,w7] = ode45(@proj,tspan,r0,opts,g,L,gamma(8));
+[t8,w8] = ode45(@proj,tspan,r0,opts,g,L,gamma(9));
+[t9,w9] = ode45(@proj,tspan,r0,opts,g,L,gamma(10));
+[t10,w10] = ode45(@proj,tspan,r0,opts,g,L,gamma(11));
+
+
+ind0 = find(w0(:,2).*circshift(w0(:,2), [-1 0]) <= 0);
+ind1 = find(w1(:,2).*circshift(w1(:,2), [-1 0]) <= 0);
+ind2 = find(w2(:,2).*circshift(w2(:,2), [-1 0]) <= 0);
+ind3 = find(w3(:,2).*circshift(w3(:,2), [-1 0]) <= 0);
+ind4 = find(w4(:,2).*circshift(w4(:,2), [-1 0]) <= 0);
+ind5 = find(w5(:,2).*circshift(w5(:,2), [-1 0]) <= 0);
+ind6 = find(w6(:,2).*circshift(w6(:,2), [-1 0]) <= 0);
+ind7 = find(w7(:,2).*circshift(w7(:,2), [-1 0]) <= 0);
+ind8 = find(w8(:,2).*circshift(w8(:,2), [-1 0]) <= 0);
+ind9 = find(w9(:,2).*circshift(w9(:,2), [-1 0]) <= 0);
+ind10 = find(w10(:,2).*circshift(w10(:,2), [-1 0]) <= 0);
+
+
+ind0 = chop(ind0,4);
+ind1 = chop(ind1,4);
+ind2 = chop(ind2,4);
+ind3 = chop(ind3,4);
+ind4 = chop(ind4,4);
+ind5 = chop(ind5,4);
+ind6 = chop(ind6,4);
+ind7 = chop(ind7,4);
+ind8 = chop(ind8,4);
+ind9 = chop(ind9,4);
+ind10 = chop(ind10,4);
+
+period0 = 2*mean(diff(t1(ind0)));
+period1 = 2*mean(diff(t1(ind1)));
+period2 = 2*mean(diff(t2(ind2)));
+period3 = 2*mean(diff(t3(ind3)));
+period4 = 2*mean(diff(t4(ind4)));
+period5 = 2*mean(diff(t5(ind5)));
+period6 = 2*mean(diff(t6(ind6)));
+period7 = 2*mean(diff(t7(ind7)));
+period8 = 2*mean(diff(t8(ind8)));
+period9 = 2*mean(diff(t9(ind9)));
+period10 = 2*mean(diff(t10(ind10)));
+
+% Period vector has components which are periods, depending on damping
+% factor
+Period = [period0 period1 period2 period3 period4 period5 period6 period7 period8 period9 period10] ;
+
+
+
+
+
+% Create plots
+
+figure(1)
+subplot(3,1,1)
+plot(t0,w0(:,1),'k-',t1,w1(:,1),'c-',t2,w2(:,1),'r-',t3,w3(:,1),'g-',t4,w4(:,1),'y-')
+legend('\gamma = 0','\gamma = 0.5','\gamma = 1','\gamma = 2','\gamma = 3')
+title('Angle vs. Time for Different \gamma ''s')
+xlabel('t')
+ylabel('\theta')
+ylim([-1 1])
+
+subplot(3,1,2)
+plot(gamma,Period,'k-')
+title('Period vs. \gamma')
+xlabel('T')
+ylabel('\gamma')
+
+AngFreq = zeros(1,11) ;
+for i = 1:11
+    AngFreq(i) = 2*pi./Period(i) ;
+end
+
+subplot(3,1,3)
+plot(gamma,AngFreq,'k-')
+title('Angular Frequency vs. \gamma')
+xlabel('\omega')
+ylabel('\gamma')
+
+
+
+figure(2)
+plot(t1,E1,'k',t4,E4,'c',t6,E6,'g',t8,E8,'y',t10,E10,'r')
+legend('\gamma = 0.5','\gamma = 2','\gamma = 4','\gamma = 6','\gamma = 8')
+title('Total Energy')
+xlabel('t')
+ylabel('Energy')
+
+figure(3)
+plot(w1(:,1),w1(:,2),'k-',w4(:,1),w4(:,2),'c-',w6(:,1),w6(:,2),'g-',w8(:,1),w8(:,2),'r-',w10(:,1),w10(:,2),'y-')
+legend('\gamma = 0.5','\gamma = 2','\gamma = 4','\gamma = 6','\gamma = 8')
+title('Phase Space')
+xlabel('\theta')
+ylabel('\theta dot')
+xlim([-1.5 1.5])
+ylim([-3 3])
+
+
+
+
+
+
+
+
+
+end
+%-------------------------------------------
+
+
+
+%t = x-axis
+%r = column vector: [theta; theta_velocity]
+%g = 9.8
+%L = length of pendulum
+function rdot = proj(t,r,g,L,gamma,A,omega)
+    rdot = [r(2); -g/L*sin(r(1)) - gamma*r(2) + A*cos(omega*t)];
+end
