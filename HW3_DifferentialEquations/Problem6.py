defe0488116540c5c299d5777b652d8b0c9f1e2b
osmaanshahid <shahid.o@husky.neu.edu>
2018-10-19 15:12:55 -0400

    Working on Problems 6 and 7.

--- HW3_DifferentialEquations/Problem6.py
+++ HW3_DifferentialEquations/Problem6.py
@@ -3,53 +3,113 @@ from matplotlib import pyplot
 import matplotlib.animation as animation
 from JSAnimation.IPython_display import display_animation
 
-K = 0.12             # cal/(sec * cm * C) 
-c = 0.113            # cal/(g * C)
-rho = 7.8            # g/(cm^3)
-alpha = K/(c*rho)    # (cm^3)/g
+# constants for iron
+Ki = 0.49             # cal/(sec * cm * C) 
+ci = 0.217            # cal/(g * C)
+rhoi = 2.7            # g/(cm^3)
+alphai = Ki/(ci*rhoi)    # (cm^3)/g
+
+
+# constants for aluminum
+Ka = 0.49             # cal/(sec * cm * C) 
+ca = 0.217            # cal/(g * C)
+rhoa = 2.7            # g/(cm^3)
+alphaa = Ka/(ca*rhoa)    # (cm^3)/g
 
 L = 50               # length of the bar, in cm
 dx = 0.2             # space step, in cm
 nx = int(L/dx)       # number of points in space
 
-x = np.arange(0,L+dx,dx) # the +1 is necessary to store the value at l
+xi = np.arange(0,L+dx,dx) # the +1 is necessary to store the value at l
+xa = np.arange(0,L+dx,dx) # the +1 is necessary to store the value at l
 dt = 0.015           # time step, in sec
-C = dx**2/dt         # 
-r = alpha/C          # stability
+C = dx**2/dt         # should be > 0.5
+ri = alphai/C          # stability
+ra = alphaa/C
 
-print(r)             # should be less than or equal to 0.5 
+print(ri,ra,C)           # should be less than or equal to 0.5 
+
+# for iron
+t0_i = np.zeros(nx+1)
+t1_i = np.zeros(nx+1)  # these arrays will contain the new displacements at t, and t+delta
+
+# for aluminum
+t0_a = np.zeros(nx+1)
+t1_a = np.zeros(nx+1)  # these arrays will contain the new displacements at t, and t+delta
+
+#Initial conditions for iron
+t0_i[:] = 100.         # initial temperature across bar
+t0_i[0] = 0.           # initial temperature at x = 0
+t0_i[nx] = 0.          # initial temperature at x = L
+
+#Initial conditions for aluminum
+t0_a[:] = 50.         # initial temperature across bar
+t0_a[0] = 0.           # initial temperature at x = 0
+t0_a[nx] = 0.          # initial temperature at x = L
+
+fig, (ax1, ax2) = pyplot.subplots(2,1)
+
+# initialize points as empty vectors
+points_i, = ax1.plot([], [], marker='', linestyle='-', lw=3)
+points_a, = ax2.plot([], [], marker='', linestyle='-', lw=3)
+points = [points_i, points_a]
 
 
-t0 = np.zeros(nx+1)
-t1 = np.zeros(nx+1)  # these arrays will contain the new displacements at t, and t+delta
-
-#Initial conditions
-t0[:] = 100.         # initial temperature across bar
-t0[0] = 0.           # initial temperature at x = 0
-t0[nx] = 0.          # initial temperature at x = L
-
-
-fig = pyplot.figure()    # open up a figure
+fig1 = pyplot.figure()    # open up a figure
 ax = pyplot.axes(xlim=(0, L), ylim=(0, 120), xlabel='x', ylabel='T')   # define axes
-points, = ax.plot([], [], marker='', linestyle='-', lw=3)     # define point style
+points_i, = ax.plot([], [], marker='', linestyle='-', lw=3)     # define point style
 
-def evolve():
-    global t0, t1
-    for ix in range(1,nx):    # updat temperature for each value of x along the bar
-        t1[ix] = t0[ix] + r*(t0[ix+1]+t0[ix-1]-2*t0[ix])   # store new temp in t1 
+fig2 = pyplot.figure()    # open up a figure
+ax = pyplot.axes(xlim=(0, L), ylim=(0, 120), xlabel='x', ylabel='T')   # define 
+points_a, = ax.plot([], [], marker='', linestyle='-', lw=3)     # define point style
 
-    points.set_data(x, t0)
+# time evolution for iron bar
+def evolve(i):
+    global t0_i, t1_i
+    global t0_a, t1_a
+    
+    for ix in range(1,nx):    # update temperature for each value of x along the bar
+        t1_i[ix] = t0_i[ix] + ri*(t0_i[ix+1]+t0_i[ix-1]-2*t0_i[ix])   # store new temp in t1 
+        t1_a[ix] = t0_a[ix] + ra*(t0_a[ix+1]+t0_a[ix-1]-2*t0_a[ix])   # store new temp in t1 
 
+    points_i.set_data(xi, t0_i)
+    points_a.set_data(xi, t0_a)
+    
     for ix in range(nx):      # for each value of x along the bar
-        t0[ix] = t1[ix]       # replace old temp in t0 with new temp from t1
+        t0_i[ix] = t1_i[ix]       # replace old temp in t0 with new temp from t1
+        t0_a[ix] = t1_a[ix]       # replace old temp in t0 with new temp from t1
+    
+    return points_i,
+    return points_a,
 
-    return points
+
+animi = animation.FuncAnimation(fig, evolve, frames = 200, interval=10)
+# anima = animation.FuncAnimation(fig, evolve_a, init_func=init_a, frames = 200, interval=10, blit=True)
 
 
-anim = animation.FuncAnimation(fig, evolve, frames = 2000, interval=10)
+pyplot.show()
 
-display_animation(anim, default_mode='once')
+
+
+
+
+
+
+
+
+
+# display_animation(anim, default_mode='once')
 
 #for i in range(20):
 #    evolve()   
 #pyplot.plot(x, t0, color='blue', ls='-', lw=3);
+
+
+
+
+
+
+
+
+
+
---
+++ HW3_DifferentialEquations/Problem7.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 19 11:29:20 2018
+
+@author: Osmaan
+"""
+
