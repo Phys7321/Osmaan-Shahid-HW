a659279a6af032d48d16bb22ba4ea42f9443e054
osmaanshahid <shahid.o@husky.neu.edu>
2018-10-21 15:36:20 -0400

    Added lots of comments to Problem 2.3, removed unnecessary drag force from end of problem.

--- HW3_DifferentialEquations/ChallengeProblems/ChallengeProblem2_3.py
+++ HW3_DifferentialEquations/ChallengeProblems/ChallengeProblem2_3.py
@@ -31,7 +31,7 @@ y0 = 300.                                    # Initial position
 v0 = 0.                                      # Initial velocity
 vt = 30.                                     # Terminal velocity
 k2 = g*mass/vt**2                            # drag coeff.
-dt = 0.01                                    # Time step
+dt = 0.1                                     # Time step
 
 p = particle(mass, y0, v0)
 
@@ -60,7 +60,8 @@ vnormal_data = np.array(vnormal)
 
 #for i in range(0,t_data.size):
 #    print (i,t_data[i], y_data[i], v_data[i])
-print(v_data[-1])
+#print(v_data[-1])
+'''
 plt.plot(t_data, v_data, color="#FF0000", ls='-', lw=3)
 plt.xlabel('Time (s)')
 plt.ylabel('Velocity (m/s)')
@@ -80,11 +81,13 @@ plt.xlabel('Time (s)')
 plt.ylabel('Position (m)')
 plt.title('Position (m) vs. Time (s)')
 plt.show()
-
+'''
 
 p = particle(mass, y0, v0)
 
-y0 = list(np.linspace(1,130,130))            # Make a list of many different heights
+y0 = list(np.linspace(600000,800000,100))            # Make a list of many different heights
+
+difflist = []
 
 for i in range(0,len(y0)):                   # For each height,
     p = particle(mass,y0[i],v0)              # Initialize particle
@@ -93,7 +96,7 @@ for i in range(0,len(y0)):                   # For each height,
     vnormal = [v0]                           # Define list and then append later
     y = [y0[i]]                              # Define list and then append later
     while p.y > 0:                           # While the particle is above the ground,
-        fy = -gforce-k2*p.v*abs(p.v)         # Net force = - mg - f_drag
+        fy = -gforce#-k2*p.v*abs(p.v)         # Net force = - mg - f_drag
         p.euler(fy, dt)                      # Find new height and velocity
         y.append(p.y)                        # Append new height to old height
         v.append(p.v)                        # Append new velocity to old velocity
@@ -101,7 +104,13 @@ for i in range(0,len(y0)):                   # For each height,
         vnormal.append(vnormal[-1] - g*dt)   # Append new velocity (constant accel.)
     vimpact = v[-1]                          # Impact velocity (non-constant accel.)
     vnormalimpact = vnormal[-1]              # Impact velocity (constant accel.)
-    diff = np.abs(((vimpact - vnormalimpact)/(vnormalimpact)))*100
-    print(i,diff,vimpact,vnormalimpact,y[i]) # Print various variables
+    diff = ((vimpact - vnormalimpact)/(vimpact))*100
+    difflist.append(diff)
+    print(i,diff,vimpact,vnormalimpact,int(y0[i])) # Print various variables
 
-
+plt.figure()
+plt.plot(y0,difflist)
+plt.xlabel('Initial height (m)')
+plt.ylabel('Percent difference')
+plt.title('Percent difference vs. Initial height (m)')
+plt.show()
